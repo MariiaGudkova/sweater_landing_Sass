df369fe3453831b0242cc6b0a5d34930b83b364d
Gudkova Mariia <gudmary8@gmail.com>
2023-01-26 11:53:33 +0400

    add history component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Header from "../Header/Header";
+import Main from "../Main/Main";
 
 function App() {
   const [isOpen, setIsOpen] = React.useState(false);
-  return <Header isOpen={isOpen} setIsOpen={setIsOpen} />;
+  return (
+    <>
+      <Header isOpen={isOpen} setIsOpen={setIsOpen} />
+      <Main />
+    </>
+  );
 }
 
 export default App;
